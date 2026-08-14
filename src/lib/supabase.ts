98001eb23d214358cc56940a2f7fa33cfd09bf7a
redzimbi-gif <redzimbi@gmail.com>
2026-08-14 19:28:14 +0000

    Corrige la clé Supabase corrompue

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://peokuzlhhskecyjtgubd.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGci••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••";
-
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlb2t1emxoaHNrZWN5anRndWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MDczMzQsImV4cCI6MjEwMjI4MzMzNH0.IMQREFyW2H4dhIF-jgqAoyc1KTJms_lX7j0ADxDtZVA"
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   realtime: {
     params: { eventsPerSecond: 10 },
